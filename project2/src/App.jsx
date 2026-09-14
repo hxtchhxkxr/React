81778344662895a8c9f42ecda6c4dc13170e2317
hxtchhxkxr <yoos405@naver.com>
2026-09-14 16:45:24 +0900

    feat: 투두 앱 UI 구현

---
+++ project2/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./component/Header.jsx";
+import TodoEditor from "./component/TodoEditor.jsx";
+import TodoList from "./component/TodoList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TodoEditor />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
