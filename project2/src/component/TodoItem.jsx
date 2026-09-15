bf85c96875184daf52efd2d31a261a93c83cd70d
hxtchhxkxr <yoos405@naver.com>
2026-09-15 15:16:56 +0900

    feat: 할 일 삭제 기능 구현

--- project2/src/component/TodoItem.jsx
+++ project2/src/component/TodoItem.jsx
@@ -1,9 +1,13 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, content, isDone, createdDate, onUpdate }) => {
+const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
     onUpdate(id);
   };
+  const onClickDelete = () => {
+    onDelete(id);
+  };
+
   return (
     <div className="TodoItem">
       <div className="checkbox_col">
@@ -14,7 +18,7 @@ const TodoItem = ({ id, content, isDone, createdDate, onUpdate }) => {
         {new Date(createdDate).toLocaleDateString()}
       </div>
       <div className="btn_col">
-        <button>삭제</button>
+        <button onClick={onClickDelete}>삭제</button>
       </div>
     </div>
   );
