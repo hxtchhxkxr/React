299f779b8ed30b741b9913780f1a36f26b5be705
hxtchhxkxr <yoos405@naver.com>
2026-09-15 15:09:09 +0900

    feat: 할 일 완료 상태 변경 기능 구현

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -40,11 +40,26 @@ function App() {
     idRef.current += 1;
   };
 
+  const onUpdate = (targetId) => {
+    setTodo(
+      todo.map((it) => {
+        if (it.id === targetId) {
+          return {
+            ...it,
+            isDone: !it.isDone,
+          };
+        } else {
+          return it;
+        }
+      }),
+    );
+  };
+
   return (
     <div className="App">
       <Header />
       <TodoEditor onCreate={onCreate} />
-      <TodoList todo={todo} />
+      <TodoList todo={todo} onUpdate={onUpdate} />
     </div>
   );
 }
