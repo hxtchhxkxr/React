8889f7284aaf425b430f9b62f2682bc08b23d80d
hxtchhxkxr <yoos405@naver.com>
2026-09-15 16:14:28 +0900

    feat: 폰트 및 이미지 추가

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { getEmotionImgById } from "./util";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img alt="감정1" src={getEmotionImgById(1)} />
+      <img alt="감정1" src={getEmotionImgById(2)} />
+      <img alt="감정1" src={getEmotionImgById(3)} />
+      <img alt="감정1" src={getEmotionImgById(4)} />
+      <img alt="감정1" src={getEmotionImgById(5)} />
+    </div>
+  );
 }
 
 export default App;
