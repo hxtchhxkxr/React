8337ed73296325cdeed9a251e2e543e7fab94a5a
hxtchhxkxr <yoos405@naver.com>
2026-09-16 14:15:04 +0900

    feat: 일기 앱 공통 스타일 설정 및 테스트 화면 정리

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,18 +1,7 @@
 import Editor from "../component/Editor";
 
 const Home = () => {
-  return (
-    <div>
-      <Editor
-        initData={{
-          date: new Date().getTime(),
-          emotionId: 1,
-          content: "이전에 작성했던 일기",
-        }}
-        onSubmit={() => alert("작성 완료!")}
-      />
-    </div>
-  );
+  return <div>Home 페이지입니다</div>;
 };
 
 export default Home;
