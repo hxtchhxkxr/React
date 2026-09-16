e124dd99dc10091587e3b2ccc1a65b2d63f5a792
hxtchhxkxr <yoos405@naver.com>
2026-09-16 10:46:34 +0900

    feat: 공통 Button 및 Header 컴포넌트 구현

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
+import Header from "../component/Header";
 
 const Home = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("sort"));
-  return <div>Home 페이지입니다</div>;
+  return (
+    <div>
+      <Header
+        title={"Home"}
+        leftChild={
+          <Button
+            type="positive"
+            text={"긍정 버튼"}
+            onClick={() => {
+              alert("positive button");
+            }}
+          />
+        }
+        rightChild={
+          <Button
+            type="negative"
+            text={"부정 버튼"}
+            onClick={() => {
+              alert("negative button");
+            }}
+          />
+        }
+      ></Header>
+    </div>
+  );
 };
 export default Home;
