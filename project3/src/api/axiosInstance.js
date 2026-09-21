f0a7fff91a557126a2cd09e6df0a73180f5e5165
hxtchhxkxr <yoos405@naver.com>
2026-09-21 10:01:44 +0900

    refactor: API 서버 주소를 개발 및 운영 환경변수로 분리

--- project3/src/api/axiosInstance.js
+++ project3/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, clearTokens } from "./token";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
 });
 
